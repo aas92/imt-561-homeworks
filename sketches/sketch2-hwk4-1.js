ec95b9b5a02d06eb23bb4d5e0e174bede1b2eba5
aaron <aarons92@uw.edugit config --global user.email aarons92@uw.edugit config --global user.name aarongit configmkdir git-practice>
2026-04-28 12:15:30 -0700

    Update Name

--- sketches/sketch2-hwk4-1.js
+++ sketches/sketch2-hwk4-1.js
@@ -12,7 +12,7 @@ registerSketch('sk2', function (p) {
     p.fill(100, 150, 240);
     p.textSize(32);
     p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. A', p.width / 2, p.height / 2);
+    p.text('Testing Title Change', p.width / 2, p.height / 2);
 
     // Draw frame as part of the sketch output.
     p.noFill();
